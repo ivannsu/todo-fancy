896d969c61a4328f07c3e38eb0e2baf0088e24c1
ivan <ivansu.webmail@gmail.com>
2018-09-08 19:58:00 +0700

    define user model, and added new static method (findOneAndCreate)

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String
+  },
+  loginType: {
+    type: String
+  },
+  todos: {
+    type: Schema.Types.ObjectId,
+    ref: 'Todo'
+  }
+}, {
+  timestamps: true
+});
+
+userSchema.statics.findOneAndCreate = function(condition, input) {
+  return new Promise((resolve, reject) => {
+    this.findOne(condition)
+    .then(user => {
+      if(!user) {
+        return this.create(input);
+      } else {
+        reject(false);
+      }
+    })
+    .then(newUser => {
+      resolve(user);
+    })
+    .catch(err => {
+      reject(err);
+    });
+  });
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
