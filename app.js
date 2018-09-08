3d58e0ef349e1bb083e431cf6b668c6d166ccc6d
ivan <ivansu.webmail@gmail.com>
2018-09-08 16:35:24 +0700

    Feature: create new todo

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Connected to mongoose...'));
 
+const todoRoutes = require('./routes/todoRoutes');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use('/todos', todoRoutes);
 
 app.listen(3000, () => console.log('Connected to server...'));
